dd198db792b6b91ddc4b4bf54126b630a08b5210
dlamli <dannylamli@hotmail.com>
2024-10-07 14:23:32 -0600

    [feat]Add SideBar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,13 @@
 import NavBar from "@/components/NavBar";
+import SideBar from "@/components/SideBar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
       <NavBar />
+      <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+        <SideBar />
+      </div>
       <main className="md:pl-20 pt-16 h-full">{children}</main>
     </div>
   );
