d5b1224678ed37f626844b40cc3db472dc610927
dlamli <dannylamli@hotmail.com>
2024-10-07 12:34:19 -0600

    [feat]Initial base & Add Clerk Auth pipeline

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+const RootPage = () => {
+  return (
+    <div>
+      <UserButton afterSwitchSessionUrl="/" />
+    </div>
+  );
+};
+
+export default RootPage;
